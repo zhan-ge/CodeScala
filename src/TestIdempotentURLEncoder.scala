83970ddea543620cc77b0491df934d04ecd41d0b
张志广 <zhange.zzg@gmail.com>
2015-07-20 16:24:03 +0800

    Update.

---
+++ src/TestIdempotentURLEncoder.scala
@@ -0,0 +1,11 @@
+
+// Created by ZG on 15/7/20.
+//
+
+import IdempotentURLEncoder._
+
+object TestIdempotentURLEncoder{
+  def main(args: Array[String]) {
+    val str = encode("http://ja.wikipedia.org/wiki/文字")
+  }
+}
